5189b342f332113ad761560cb437abbfeef84984
Dariusz Korolczuk <dkorolczuk86@gmail.com>
2018-09-29 18:33:52 +0100

    tutorial API in php

---
+++ api/product/delete.php
@@ -0,0 +1,39 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// get database connection
+include_once '../config/database.php';
+ 
+// instantiate product object
+include_once '../objects/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$product->name = $data->name;
+ 
+// update the product
+if($product->delete()){
+    echo '{';
+        echo '"message": "Product was deleted."';
+    echo '}';
+}
+ 
+// if unable to update the product, tell the user
+else{
+    echo '{';
+        echo '"message": "Unable to delete product."';
+    echo '}';
+}
+?>
