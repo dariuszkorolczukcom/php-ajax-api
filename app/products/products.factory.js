ea5aa94e840755ef0dd7ef729a1f146b07ebbf5a
Dariusz Korolczuk <dkorolczuk86@gmail.com>
2018-09-30 16:23:32 +0100

    read one and edit

--- app/products/products.factory.js
+++ app/products/products.factory.js
@@ -10,7 +10,45 @@ app.factory("productsFactory", function($http){
         });
     };
      
-    // createProduct will be here
+    // create product
+factory.createProduct = function($scope){
+    return $http({
+        method: 'POST',
+        data: {
+            'name' : $scope.name,
+            'description' : $scope.description,
+            'price' : $scope.price,
+            'category_id' : 1
+        },
+        url: 'http://localhost/php-api-codeofaninja/api/product/create.php'
+    });
+};
+ 
+// read one product
+factory.readOneProduct = function(id){
+    return $http({
+        method: 'GET',
+        url: 'http://localhost/php-api-codeofaninja/api/product/read_one.php?id=' + id
+    });
+};
+ 
+// update product
+factory.updateProduct = function($scope){
+ 
+    return $http({
+        method: 'POST',
+        data: {
+            'id' : $scope.id,
+            'name' : $scope.name,
+            'description' : $scope.description,
+            'price' : $scope.price,
+            'category_id' : 1
+        },
+        url: 'http://localhost/php-api-codeofaninja/api/product/update.php'
+    });
+};
+ 
+// deleteProduct will be here
      
     return factory;
 });
