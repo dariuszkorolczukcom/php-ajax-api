1b787846b8e7a09a621d8c41be69f0dda6c4921d
Dariusz Korolczuk <dkorolczuk86@gmail.com>
2018-09-30 16:31:51 +0100

    delet

--- app/products/products.factory.js
+++ app/products/products.factory.js
@@ -1,63 +1,69 @@
-app.factory("productsFactory", function($http){
- 
+app.factory("productsFactory", function ($http) {
+
     var factory = {};
- 
+
     // read all products
-    factory.readProducts = function(){
+    factory.readProducts = function () {
         return $http({
             method: 'GET',
             url: 'http://localhost/php-api-codeofaninja/api/product/read.php'
         });
     };
-     
+
     // create product
-factory.createProduct = function($scope){
-    return $http({
-        method: 'POST',
-        data: {
-            'name' : $scope.name,
-            'description' : $scope.description,
-            'price' : $scope.price,
-            'category_id' : 1
-        },
-        url: 'http://localhost/php-api-codeofaninja/api/product/create.php'
-    });
-};
- 
-// read one product
-factory.readOneProduct = function(id){
-    return $http({
-        method: 'GET',
-        url: 'http://localhost/php-api-codeofaninja/api/product/read_one.php?id=' + id
-    });
-};
- 
-// update product
-factory.updateProduct = function($scope){
- 
-    return $http({
-        method: 'POST',
-        data: {
-            'id' : $scope.id,
-            'name' : $scope.name,
-            'description' : $scope.description,
-            'price' : $scope.price,
-            'category_id' : 1
-        },
-        url: 'http://localhost/php-api-codeofaninja/api/product/update.php'
-    });
-};
- 
-// delete product
-factory.deleteProduct = function(id){
-    return $http({
-        method: 'POST',
-        data: { 'id' : id },
-        url: 'http://localhost/php-api-codeofaninja/api/product/delete.php'
-    });
-};
- 
-// searchProducts will be here
-     
+    factory.createProduct = function ($scope) {
+        return $http({
+            method: 'POST',
+            data: {
+                'name': $scope.name,
+                'description': $scope.description,
+                'price': $scope.price,
+                'category_id': 1
+            },
+            url: 'http://localhost/php-api-codeofaninja/api/product/create.php'
+        });
+    };
+
+    // read one product
+    factory.readOneProduct = function (id) {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost/php-api-codeofaninja/api/product/read_one.php?id=' + id
+        });
+    };
+
+    // update product
+    factory.updateProduct = function ($scope) {
+
+        return $http({
+            method: 'POST',
+            data: {
+                'id': $scope.id,
+                'name': $scope.name,
+                'description': $scope.description,
+                'price': $scope.price,
+                'category_id': 1
+            },
+            url: 'http://localhost/php-api-codeofaninja/api/product/update.php'
+        });
+    };
+
+    // delete product
+    factory.deleteProduct = function (id) {
+        return $http({
+            method: 'POST',
+            data: { 'id': id },
+            url: 'http://localhost/php-api-codeofaninja/api/product/delete.php'
+        });
+    };
+
+    // search all products
+    factory.searchProducts = function (keywords) {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost/php-api-codeofaninja/api/product/search.php?s=' + keywords
+        });
+    };
+
     return factory;
 });
